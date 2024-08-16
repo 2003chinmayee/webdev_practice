6e02c7f8e5664799497e0cf32d73282a2a24cc75
CHINMAYEE PATIL <155649544+2003chinmayee@users.noreply.github.com>
2024-08-16 13:07:23 +0530

    Add files via upload

---
+++ javascript70/script.js
@@ -0,0 +1,154 @@
+function bb1() {
+    let a = Math.floor(Math.random() * 3);
+
+    switch (a) {
+        case 0:
+            document.getElementById("box1").style.color = "green";
+            break;
+        case 1:
+            document.getElementById("box1").style.color = "yellow";
+            break;
+        case 2:
+            document.getElementById("box1").style.color = "red";
+            break;
+    }
+
+    let b = Math.floor(Math.random() * 3);
+    switch (b) {
+        case 0:
+            document.getElementById("box1").style.backgroundColor = "aqua";
+            break;
+        case 1:
+            document.getElementById("box1").style.backgroundColor = "pink";
+            break;
+        case 2:
+            document.getElementById("box1").style.backgroundColor = "blue";
+            break;
+    }
+}
+
+function bb2() {
+    let a = Math.floor(Math.random() * 3);
+
+    switch (a) {
+        case 0:
+            document.getElementById("box2").style.color = "green";
+            break;
+        case 1:
+            document.getElementById("box2").style.color = "yellow";
+            break;
+        case 2:
+            document.getElementById("box2").style.color = "red";
+            break;
+    }
+
+    let b = Math.floor(Math.random() * 3);
+    switch (b) {
+        case 0:
+            document.getElementById("box2").style.backgroundColor = "aqua";
+            break;
+        case 1:
+            document.getElementById("box2").style.backgroundColor = "pink";
+            break;
+        case 2:
+            document.getElementById("box2").style.backgroundColor = "blue";
+            break;
+    }
+}
+
+function bb3() {
+    let a = Math.floor(Math.random() * 3);
+
+    switch (a) {
+        case 0:
+            document.getElementById("box3").style.color = "green";
+            break;
+        case 1:
+            document.getElementById("box3").style.color = "yellow";
+            break;
+        case 2:
+            document.getElementById("box3").style.color = "red";
+            break;
+    }
+
+    let b = Math.floor(Math.random() * 3);
+    switch (b) {
+        case 0:
+            document.getElementById("box3").style.backgroundColor = "aqua";
+            break;
+        case 1:
+            document.getElementById("box3").style.backgroundColor = "pink";
+            break;
+        case 2:
+            document.getElementById("box3").style.backgroundColor = "blue";
+            break;
+    }
+}
+
+function bb4() {
+    let a = Math.floor(Math.random() * 3);
+
+    switch (a) {
+        case 0:
+            document.getElementById("box4").style.color = "green";
+            break;
+        case 1:
+            document.getElementById("box4").style.color = "yellow";
+            break;
+        case 2:
+            document.getElementById("box4").style.color = "red";
+            break;
+    }
+
+    let b = Math.floor(Math.random() * 3);
+    switch (b) {
+        case 0:
+            document.getElementById("box4").style.backgroundColor = "aqua";
+            break;
+        case 1:
+            document.getElementById("box4").style.backgroundColor = "pink";
+            break;
+        case 2:
+            document.getElementById("box4").style.backgroundColor = "blue";
+            break;
+    }
+}
+
+function bb5() {
+    let a = Math.floor(Math.random() * 3);
+
+    switch (a) {
+        case 0:
+            document.getElementById("box5").style.color = "green";
+            break;
+        case 1:
+            document.getElementById("box5").style.color = "yellow";
+            break;
+        case 2:
+            document.getElementById("box5").style.color = "red";
+            break;
+    }
+
+    let b = Math.floor(Math.random() * 3);
+    switch (b) {
+        case 0:
+            document.getElementById("box5").style.backgroundColor = "aqua";
+            break;
+        case 1:
+            document.getElementById("box5").style.backgroundColor = "pink";
+            break;
+        case 2:
+            document.getElementById("box5").style.backgroundColor = "blue";
+            break;
+    }
+}
+
+
+// Call the functions
+bb1();
+bb2();
+bb3();
+bb4();
+bb5();
+
+console.log("Functions executed");
